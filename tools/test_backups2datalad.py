e547f31b57dec1adf2797724388737ef84e03181
John T. Wodder II <git@varonathe.org>
2021-08-06 10:26:42 -0400

    Fill in some things

--- tools/test_backups2datalad.py
+++ tools/test_backups2datalad.py
@@ -1,7 +1,4 @@
-from backups2datalad import DandiDatasetter
-from datalad.api import Dataset
 from pathlib import Path
-
 
 import pytest
 
@@ -13,20 +10,21 @@ from dandi.tests.fixtures import (
 )
 
 # needs  nose
-from datalad.tests.utils import (
-    assert_repo_status,
-    ok_file_under_git
-)
+from datalad.api import Dataset
+from datalad.tests.utils import assert_repo_status, ok_file_under_git
+
+from backups2datalad import DandiDatasetter
 
 
-def test_1(text_dandiset, tmpdir):
+def test_1(text_dandiset, tmp_path):
     # TODO: move pre-setup into a fixture, e.g. local_setup1 or make code work without?
-    assetstore_path = tmpdir / "assetstore"
-    assetstore_path.mkdir().mkdir("girder-assetstore")
-    target_path = Path(tmpdir / "target")
+    assetstore_path = tmp_path / "assetstore"
+    assetstore_path.mkdir()
+    (assetstore_path / "girder-assetstore").mkdir()
+    target_path = tmp_path / "target"
     di = DandiDatasetter(
         dandi_client=text_dandiset["client"],
-        assetstore_path=Path(assetstore_path),
+        assetstore_path=assetstore_path,
         target_path=target_path,
         ignore_errors=False,
         # gh_org=None,
@@ -35,11 +33,11 @@ def test_1(text_dandiset, tmpdir):
         # jobs=jobs,
         # force=force,
         # update_asset_meta_version=update_asset_meta_version,
-        content_url_regex=r".*/blobs/"
+        content_url_regex=r".*/blobs/",
     )
     # some exception is raised whenever we try to update non-existing dandiset
     with pytest.raises(Exception):
-        di.update_from_backup(['000999'])
+        di.update_from_backup(["000999"])
 
     # TODO: fix -- should not initiate dataset unless "valid"
     # assert not (target_path / "000999").exists()
@@ -48,38 +46,38 @@ def test_1(text_dandiset, tmpdir):
     # TODO: may be separate it out, so we could start "clean" and still work ok
     # clean run without dandisets is ok
     # ret = di.update_from_backup()
-    # assert ret is None,  "nothing is returned ATM, if added -- test should be extended"
+    # assert ret is None, "nothing is returned ATM, if added -- test should be extended"
 
     di.update_from_backup()
 
-    ds = Dataset(target_path / text_dandiset["dandiset_id"])  # but we should get the super-dataset?
+    ds = Dataset(
+        target_path / text_dandiset["dandiset_id"]
+    )  # but we should get the super-dataset?
     assert_repo_status(ds.path)  # that all is clean etc
+    ok_file_under_git(ds.path, "file.txt")
 
-    # TODO: check that files were committed, ok_file_under_git could be of help
-
-    # TODO: add changes to the dandiset
-
-    # reupload dandiset
+    (text_dandiset["dspath"] / "new.txt").write_text("This is a new file.\n")
     text_dandiset["reupload"]()
-
     assert_repo_status(ds.path)  # no side-effects somehow
-
     di.update_from_backup()
     assert_repo_status(ds.path)  # that all is clean etc
+    assert (ds.path / "new.txt").read_text() == "This is a new file.\n"
 
-    # TODO: check that added file was uploaded/fetched
-
-    # TODO: publish the dandiset
-    version1 = '???'
-
+    version1 = text_dandiset["dandiset"].publish().version.identifier
     di.update_from_backup()
     assert_repo_status(ds.path)  # that all is clean etc
     assert version1 in ds.repo.get_tags()
 
-    # TODO: more modifications to draft
-
+    (text_dandiset["dspath"] / "new.txt").write_text(
+        "This file's contents were changed.\n"
+    )
+    text_dandiset["reupload"]()
     di.update_from_backup()
     assert_repo_status(ds.path)  # that all is clean etc
-    # TODO: check modifications being in place
+    assert (ds.path / "new.txt").read_text() == "This file's contents were changed.\n"
 
-    # TODO: another publish and another check for now both tags being present
+    version2 = text_dandiset["dandiset"].publish().version.identifier
+    di.update_from_backup()
+    assert_repo_status(ds.path)  # that all is clean etc
+    assert version1 in ds.repo.get_tags()
+    assert version2 in ds.repo.get_tags()
