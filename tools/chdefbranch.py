9ae1d2b5c8f199c2793fb42554ae6edfcae0c137
John T. Wodder II <git@varonathe.org>
2021-08-18 14:06:03 -0400

    Script for changing default branch

---
+++ tools/chdefbranch.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+__requires__ = ["click >= 8.0.1", "PyGithub ~= 1.53"]
+from pathlib import Path
+from subprocess import check_output, run
+import click
+from github import Github
+
+
+@click.command()
+@click.argument("organization")
+@click.argument(
+    "datasets", type=click.Path(file_okay=False, exists=True, path_type=Path), nargs=-1
+)
+def main(organization, datasets):
+    """
+    Change default branch from 'master' to 'draft'.
+
+    Run as ``python chdefbranch.py ORG DATASET ...`` where ``ORG`` is the name
+    of the GitHub organization to which the GitHub repositories belong and each
+    ``DATASET`` is a path to a local clone of a repository to update.
+    """
+    token = check_output(["git", "config", "hub.oauthtoken"], text=True).strip()
+    gh = Github(token)
+    org = gh.get_organization(organization)
+    for ds in datasets:
+        repo = org.get_repo(ds.name)
+        print(repo.full_name)
+        # <https://stackoverflow.com/a/55020304/744178>
+        master = repo.get_git_ref("heads/master")
+        repo.create_git_ref("refs/heads/draft", sha=master.object.sha)
+        repo.edit(default_branch="draft")
+        master.delete()
+
+        def git(*args):
+            run(["git", *args], cwd=ds, check=True)
+
+        git("branch", "-m", "master", "draft")
+        git("fetch", "github")
+        git("branch", "-u", "github/draft", "draft")
+        git("remote", "set-head", "github", "-a")
+        git("remote", "prune", "github")
+
+
+if __name__ == "__main__":
+    main()
