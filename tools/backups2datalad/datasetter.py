a90cb9599c19de54892778cad97aa170d623eb76
John T. Wodder II <git@varonathe.org>
2022-06-22 22:12:53 -0400

    Use a lock to prevent multiple tasks from setting the GitHub client at once

--- tools/backups2datalad/datasetter.py
+++ tools/backups2datalad/datasetter.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 import json
 import logging
@@ -45,6 +45,7 @@ class DandiDatasetter(AsyncResource):
     dandi_client: AsyncDandiClient
     config: BackupConfig
     gh: Optional[httpx.AsyncClient] = None
+    ghlock: anyio.Lock = field(default_factory=anyio.Lock)
 
     async def aclose(self) -> None:
         await self.dandi_client.aclose()
@@ -426,12 +427,16 @@ class DandiDatasetter(AsyncResource):
             await ds.call_git("push", "github", dandiset.version_id)
 
     async def edit_github_repo(self, repo: GHRepo, **kwargs: Any) -> None:
-        if self.gh is None:
-            token = await areadcmd("git", "config", "hub.oauthtoken")
-            self.gh = httpx.AsyncClient(
-                headers={"Authorization": f"token {token}", "User-Agent": USER_AGENT},
-                follow_redirects=True,
-            )
+        async with self.ghlock:
+            if self.gh is None:
+                token = await areadcmd("git", "config", "hub.oauthtoken")
+                self.gh = httpx.AsyncClient(
+                    headers={
+                        "Authorization": f"token {token}",
+                        "User-Agent": USER_AGENT,
+                    },
+                    follow_redirects=True,
+                )
         log.debug("Editing repository %s", repo)
         # Retry on 404's in case we're calling this right after
         # create_github_sibling(), when the repo may not yet exist
