7811bb3d1e3415ff45463536f2bb2652e3c88caa
John T. Wodder II <git@varonathe.org>
2022-09-19 10:20:40 -0400

    Add helper script for just syncing Zarrs identified by Zarr ID

---
+++ tools/sync_zarrs_by_id.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import sys
+
+import anyio
+
+from backups2datalad.adandi import AsyncDandiClient
+from backups2datalad.config import BackupConfig, ResourceConfig
+from backups2datalad.logging import log
+from backups2datalad.manager import Manager
+from backups2datalad.zarr import sync_zarr
+
+if sys.version_info[:2] >= (3, 10):
+    from contextlib import aclosing
+else:
+    from async_generator import aclosing
+
+dandiset_id = "FILL IN HERE"
+zarr_ids = {"FILL", "IN", "HERE"}
+backup_folder = "FILL IN HERE"
+
+
+async def amain():
+    config = BackupConfig(zarrs=ResourceConfig(path="dandizarrs"))
+    async with AsyncDandiClient.for_dandi_instance("dandi") as client:
+        dandiset = await client.get_dandiset(dandiset_id, "draft")
+        async with aclosing(dandiset.aget_zarr_assets()) as ait:
+            async for asset in ait:
+                if asset.zarr in zarr_ids:
+                    print(f"SYNCING {asset.zarr} ({asset.path})")
+                    checksum = asset.get_digest().value
+                    await sync_zarr(
+                        asset,
+                        checksum,
+                        Path(backup_folder, asset.zarr),
+                        Manager(config=config, gh=None, log=log),
+                    )
+
+
+if __name__ == "__main__":
+    anyio.run(amain)
