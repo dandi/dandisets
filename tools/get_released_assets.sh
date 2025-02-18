007e26258d51578aeff4cb4677580086f6ff2878
DANDI Team <team@dandiarchive.org>
2025-02-18 15:34:58 -0500

    ignore some releases for which data was removed from S3

--- tools/get_released_assets.sh
+++ tools/get_released_assets.sh
@@ -10,6 +10,10 @@ for ds in "$@"; do
 	echo "I: $ds"
 	git tag | grep -E '^[0-9]\.[0-9]{6}\.[0-9]{4}' | sort | \
 	while read tag; do
+		# some had removed, e.g. in 000121
+		case "$tag" in
+			0.210815.0703|0.220124.2156) continue;;
+		esac
 		git annex get --branch="$tag" -J 5 --from=web
 	done
 )
